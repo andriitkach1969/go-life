a1ab8d8d29caf45aaaefead86fe74d3d49ef028d
Andrii Tkach <papa.tkach@gmail.com>
2020-02-23 15:03:43 +0200

    define neumann. fix moore

--- src/neumann.py
+++ src/neumann.py
@@ -4,14 +4,12 @@ IS_DESERT = 1
 IS_CROWD = 2
 
 
-class Moore:
+class Neumann:
 
     def __init__(self):
         self.array = 0
         self.nsize = 0
         self.msize = 0
-        self.row_pos = 0
-        self.column_pos = 0
 
     def __get_neighbors(self, arr, current_i, current_j):
         res = []
@@ -23,7 +21,7 @@ class Moore:
         sw = arr.get(current_i + 1 if current_i < self.nsize else 1, current_j - 1 if current_j > 1 else self.msize)
         s = arr.get(current_i + 1 if current_i < self.nsize else 1, current_j)
         se = arr.get(current_i + 1 if current_i < self.nsize else 1, current_j + 1 if current_j < self.msize else 1)
-        res = [nw, n, ne, e, w, sw, s, se]
+        res = [n, e, w, s]
         return res
 
     def __get_neighbors_state(self, arr, current_i, current_j):
@@ -32,12 +30,10 @@ class Moore:
         for item in neighbors:
             if item:
                 count += 1
-        if count < 2:
-            return IS_DESERT
-        elif count > 3:
-            return IS_CROWD
-        elif count == 3:
+        if count == 1:
             return IS_OK
+        else:
+            return IS_CROWD
 
     def get_comfort_level(self, arr, nsize, msize, current_i, current_j):
         self.nsize = nsize
